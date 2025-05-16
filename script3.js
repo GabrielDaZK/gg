9d2877a561d08719136b4e150dcb8068191c9cf5
GabrielDaZK <gabriel.antunes.correa@escola.pr.gov.br>
2025-05-16 09:44:55 -0300

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,51 @@
+let nu1m;
+let num2;
+let resulado;
+
+function sobra() {
+    nu1m = Number(window.prompt("digite o primeiro numero:"));
+    num2 = Number(window.prompt("digite o segundo numero:"));
+
+    resulado = num1 % num2;
+
+    alert("o resultado é: " + resulado + ".");
+}
+
+
+function multiplicacao() {
+    nu1m = Number(window.prompt("digite o primeiro numero:"));
+    num2 = Number(window.prompt("digite o segundo numero:"));
+
+    resulado = num1 * num2;
+
+    alert("o resultado é: " + resulado + ".");
+}
+
+
+function divisao() {
+    nu1m = Number(window.prompt("digite o primeiro numero:"));
+    num2 = Number(window.prompt("digite o segundo numero:"));
+
+    resulado = num1 / num2;
+
+    alert("o resultado é: " + resulado + ".");
+}
+
+
+function subtracao() {
+    nu1m = Number(window.prompt("digite o primeiro numero:"));
+    num2 = Number(window.prompt("digite o segundo numero:"));
+
+    resulado = num1 - num2;
+
+    alert("o resultado é: " + resulado + ".");
+}
+
+function adicao() {
+    nu1m = Number(window.prompt("digite o primeiro numero:"));
+    num2 = Number(window.prompt("digite o segundo numero:"));
+
+    resulado = num1 + num2;
+
+    alert("o resultado é: " + resulado + ".");
+}
